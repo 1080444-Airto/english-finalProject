3a49f22674e67ba3d4bb841099bbf58c63fc28fb
1080444-Airto <1080444@hr.nl>
2024-12-30 22:44:22 +0100

    second level done and endscreen created

--- routes/routers.js
+++ routes/routers.js
@@ -20,11 +20,25 @@ router.get("/level-one", auth, (req, res) => {
     return res.render("levelOne")
 })
 
+router.get("/level-two", auth, (req, res) => {
+    return res.render("levelTwo")
+})
+
+router.get("/end-game", auth, (req, res) => {
+    return res.render("endScreen")
+})
+
 //All post Routes
 router.post("/login", (req, res) => {
     req.session.username = req.body.username.trim()
 
     return res.redirect("/level-one")
+})
+
+router.post("/logout", auth, (req, res) => {
+    req.session.username = null
+
+    return res.redirect("/")
 })
 
 module.exports = router
